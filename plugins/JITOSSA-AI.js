99afdcbaa8339b6d9976a87372a056b8d22f8d4e
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-13 21:27:42 +0000

    Update JITOSSA-AI.js

--- plugins/JITOSSA-AI.js
+++ plugins/JITOSSA-AI.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-let welcomedUsers = {}; // Object to track welcomed users
-
 let handler = async (m, { conn }) => {
     conn.autoai = conn.autoai ? conn.autoai : {};
 
@@ -9,18 +7,10 @@ let handler = async (m, { conn }) => {
     if (!m.text || m.isBaileys || m.text.includes("http://") || m.text.includes("https://")) return;
 
     let name = "JITOSSA AI";
+    await conn.sendMessage(m.chat, { react: { text: `⏱️`, key: m.key }});
 
-    // التأكد من أن الشخص لم يتم الترحيب به من قبل
-    if (!welcomedUsers[m.sender]) {
-        welcomedUsers[m.sender] = 1; // تعيين قيمة 1 للشخص الذي تم الترحيب به
-
-        await conn.send(m.chat, `مرحبًا بك في JITOSSA AI، أنا هنا لمساعدتك. يرجى كتابة رسالتك للبدء في المحادثة.`);
-
-        return; // توقف عن تنفيذ الكود للمرة الأولى
-    }
-
-    // الرد تلقائيًا على الرسائل العادية
     const messages = [
+        { role: "system", content: `أنا بوت واتساب، اسمي ${name}` },
         { role: "user", content: m.text }
     ];
 
@@ -30,7 +20,36 @@ let handler = async (m, { conn }) => {
         });
         const responseData = response.data;
         const hasil = responseData;
-        await conn.send(m.chat, hasil.answer);
+        await conn.sendMessage(m.chat, { react: { text: `✅`, key: m.key }});
+        m.reply(hasil.answer);
+    } catch (error) {
+        console.error("حدث خطأ أثناء جلب البيانات:", error);
+        throw error;
+    }
+}
+
+handler.before = async (m, { conn }) => {
+    conn.autoai = conn.autoai ? conn.autoai : {};
+
+    // تجاهل الرسائل الفارغة أو التي تحتوي على روابط
+    if (!m.text || m.isBaileys || m.text.includes("http://") || m.text.includes("https://")) return;
+
+    let name = "JITOSSA AI";
+    await conn.sendMessage(m.chat, { react: { text: `⏱️`, key: m.key }});
+
+    const messages = [
+        { role: "system", content: `أنا بوت واتساب، اسمي ${name}` },
+        { role: "user", content: m.text }
+    ];
+
+    try {
+        const response = await axios.post("https://deepenglish.com/wp-json/ai-chatbot/v1/chat", {
+            messages
+        });
+        const responseData = response.data;
+        const hasil = responseData;
+        await conn.sendMessage(m.chat, { react: { text: `✅`, key: m.key }});
+        m.reply(hasil.answer);
     } catch (error) {
         console.error("حدث خطأ أثناء جلب البيانات:", error);
         throw error;
