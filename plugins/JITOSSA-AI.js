99ff67a62d0aabbc34b47e8367aca6042da85c1e
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-13 22:45:34 +0000

    Update JITOSSA-AI.js

--- plugins/JITOSSA-AI.js
+++ plugins/JITOSSA-AI.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const deepaiApiKey = '4c18ae2e-9978-4c6c-a5ff-543d11dd8866';
-
 let handler = async (m, { conn }) => {
+    conn.autoai = conn.autoai ? conn.autoai : {};
+
     // تجاهل الرسائل الفارغة أو التي تحتوي على روابط
     if (!m.text || m.isBaileys || m.text.includes("http://") || m.text.includes("https://")) return;
 
@@ -13,19 +13,45 @@ let handler = async (m, { conn }) => {
     ];
 
     try {
-        const response = await axios({
-            method: 'post',
-            url: 'https://api.deepai.org/api/text-generator',
+        const response = await axios.post("https://api.deepai.org/api/text-generator", {
+            text: messages.map(msg => `${msg.role}: ${msg.content}`).join('\n'),
+            type: 'text-davinci-003',
+            max_tokens: 150
+        }, {
             headers: {
-                'Api-Key': deepaiApiKey
-            },
-            data: {
-                text: messages.map(msg => `${msg.role}: ${msg.content}`).join('\n'),
-                type: 'text-davinci-003',
-                max_tokens: 150
+                'Api-Key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'
             }
         });
-        
+        const hasil = response.data.output.trim();
+        m.reply(hasil);
+    } catch (error) {
+        console.error("حدث خطأ أثناء جلب البيانات:", error);
+        throw error;
+    }
+}
+
+handler.before = async (m, { conn }) => {
+    conn.autoai = conn.autoai ? conn.autoai : {};
+
+    // تجاهل الرسائل الفارغة أو التي تحتوي على روابط
+    if (!m.text || m.isBaileys || m.text.includes("http://") || m.text.includes("https://")) return;
+
+    let name = "JITOSSA AI";
+    const messages = [
+        { role: "system", content: `أنا بوت واتساب، اسمي ${name}` },
+        { role: "user", content: m.text }
+    ];
+
+    try {
+        const response = await axios.post("https://api.deepai.org/api/text-generator", {
+            text: messages.map(msg => `${msg.role}: ${msg.content}`).join('\n'),
+            type: 'text-davinci-003',
+            max_tokens: 150
+        }, {
+            headers: {
+                'Api-Key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'
+            }
+        });
         const hasil = response.data.output.trim();
         m.reply(hasil);
     } catch (error) {
