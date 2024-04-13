7613990047870b526eaf57262ac9787c7c5d6f1e
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-13 20:53:58 +0000

    Update JITOSSA-AI.js

--- plugins/JITOSSA-AI.js
+++ plugins/JITOSSA-AI.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 let handler = async (m, { conn }) => {
     conn.autoai = conn.autoai ? conn.autoai : {};
 
-    if (!m.text || m.isBaileys || hasLinks(m.text)) return; // التحقق من وجود روابط
+    if (!m.text || m.isBaileys) return;
 
     let name = conn.getName(m.sender);
-    await conn.sendMessage(m.chat, `I am a WhatsApp bot, ${name}`);
+    await conn.sendMessage(m.chat, { react: { text: `⏱️`, key: m.key }});
 
     const messages = [
         { role: "system", content: `I am a WhatsApp bot, ${name}` },
@@ -19,26 +19,40 @@ let handler = async (m, { conn }) => {
         });
         const responseData = response.data;
         const hasil = responseData;
-        await conn.sendMessage(m.chat, `Chatbot: ${hasil.answer}`);
+        await conn.sendMessage(m.chat, { react: { text: `✅`, key: m.key }});
+        m.reply(hasil.answer);
     } catch (error) {
         console.error("Error fetching data:", error);
         throw error;
     }
 }
 
-// الدالة للتحقق من وجود روابط
-function hasLinks(text) {
-    // يمكنك استخدام تعبيرات الاختبار النمطي للتحقق من وجود روابط
-    const linkPattern = /https?:\/\/\S+/i;
-    return linkPattern.test(text);
-}
-
 handler.before = async (m, { conn }) => {
-    // لا تنفذ أي شيء قبل وصول الرسالة
+    if (!m.text || m.isBaileys) return;
+
+    let name = conn.getName(m.sender);
+    await conn.sendMessage(m.chat, { react: { text: `⏱️`, key: m.key }});
+
+    const messages = [
+        { role: "system", content: `I am a WhatsApp bot, ${name}` },
+        { role: "user", content: m.text }
+    ];
+
+    try {
+        const response = await axios.post("https://deepenglish.com/wp-json/ai-chatbot/v1/chat", {
+            messages
+        });
+        const responseData = response.data;
+        const hasil = responseData;
+        await conn.sendMessage(m.chat, { react: { text: `✅`, key: m.key }});
+        m.reply(hasil.answer);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
 }
 
 handler.command = ['autoai'];
 handler.tags = ["ai"]
 handler.help = ['autoai']
-
 export default handler;
